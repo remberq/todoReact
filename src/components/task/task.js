b905254c747b8ae8b353d9ff7b610c5ffa1eaf7f
remberq <rember.out@gmail.com>
2022-06-17 23:55:11 +0300

    s

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,25 +1,23 @@
-import Input from '../input/input'
-import Button from '../button/button'
-import './task.css'
+import Input from '../input/input';
+import Button from '../button/button';
+import './task.css';
 
 const Task = ({ task, onClickedTask, completeTask }) => {
-  let selector = !task.active ? ' completed' : '' // Add new class if task completed
+  let selector = !task.active ? ' completed' : ''; // Add new class if task completed
 
   return (
     <li className={selector}>
-      <div className="view">
+      <div className='view'>
         <Input makeActive={() => completeTask(task.id)} selector={task} />
         <label>
-          <span className="description">{task.title}</span>
-          <span className="created">{task.created}</span>
+          <span className='description'>{task.title}</span>
+          <span className='created'>{task.created}</span>
         </label>
         <Button classes={'icon-edit'} />
         <Button classes={'icon-destroy'} destroy={onClickedTask} />
       </div>
     </li>
-  )
-}
+  );
+};
 
-let name = 'ad'
-
-export default Task
+export default Task;
